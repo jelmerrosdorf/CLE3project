6637df056947b117f7b00598676693e6bf69c9f4
jelmerrosdorf <jelmerr99@gmail.com>
2021-03-15 21:21:37 +0100

    Added homepage and stylesheet. Deleted index (replaced by homepage).

---
+++ homepage.php
@@ -0,0 +1,48 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="./style.css"/>
+</head>
+
+<header>
+    <h1>VR Chatroom BIRD Rotterdam</h1>
+    <img src="https://res.cloudinary.com/yungdorf/image/upload/v1615838227/club_ygfuxt.png">
+</header>
+
+<body>
+
+<section id="info chatroom">
+    <p>You asked, we delivered. Imagine not being able to see your favorite artist live because of corona. Sounds boring, right? Not anymore! Thanks to the amazing work of the Cool Ranch Wings, you can
+        now follow all BIRD events from your living room in virtual reality. No need to thank us.</p>
+</section>
+
+
+<section id="register">
+    <p>Register here to join the fun in the VR BIRD Chatroom.</p>
+</section>
+
+
+<section id="donate">
+    <p>To support BIRD you can donate here.</p>
+</section>
+
+
+</body>
+
+<footer>
+    <a href="https://bird-rotterdam.nl/">BIRD Home page</a><br>
+    <a href="https://twitter.com/Bird_Rdam">Twitter</a> | <a href="https://www.instagram.com/birdrotterdam/">Instagram</a> | <a href="https://www.facebook.com/BIRDrotterdam">Facebook</a>
+</footer>
+
+</html>
+
+
+
+
