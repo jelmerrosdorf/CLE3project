8647ee9e117e88f3f5adf87ae39a89f056078017
jelmerrosdorf <jelmerr99@gmail.com>
2021-04-06 17:50:51 +0200

    Added text and changed the homepage to be English-only

--- homepage.php
+++ homepage.php
@@ -16,9 +16,9 @@
         <div class="dropdown">
             <img src="https://res.cloudinary.com/yungdorf/image/upload/v1617715167/dropdownmenu_g4qw1b.png">
             <div class="dropdown-content">
-                <a href="slideshow.php">Album Collectie</a>
+                <a href="slideshow.php">Album Collection</a>
                 <a href="register.php">Register</a>
-                <a href="https://bird-rotterdam.nl/support-bird/">Doneren</a>
+                <a href="https://bird-rotterdam.nl/support-bird/">Donate</a>
                 <a href="#donate">Contact</a>
             </div>
         </div>
@@ -34,31 +34,33 @@
 <body>
 
 <article>
-    <a href=https://bird-rotterdam.nl/agenda/">Check hier de agenda van BIRD</a><br>
+    <a href=https://bird-rotterdam.nl/agenda/">Check all BIRD's events here</a><br>
 </article>
 
 <section id="info chatroom">
-    <p>You asked, we delivered. Imagine not being able to see your favorite artist live because of Covid 19. Sounds boring, right? Not anymore! Thanks to the amazing work of the Cool Ranch Wings, you can
+    <p>You asked, we delivered. Imagine not being able to see your favorite artist live because of Covid-19. Sounds boring, right? Not anymore! Thanks to the amazing work of the Cool Ranch Wings, you can
         now follow all BIRD events from your living room in virtual reality. No need to thank us.</p>
 </section>
 
 <article>
-    <a href=https://bird-rotterdam.nl/agenda/">Onze eigen shit nog ff fixen voor de chatroom </a><br>
+    <a>BIRD's very own VR chatroom </a><br>
 </article>
 
 <section id="register">
-    <p>Register here to join the fun in the VR BIRD Chatroom.</p>
+    <p>Our team of four set out to provide people from all ages and backgrounds with a taste of their favorite venue. Whether you like BIRD for the soothing jazz sounds, the raw hiphop beats or even just the amazing food and cozy restaurant vibe,
+    we've got you covered. Follow the link below to register for the virtual reality chatroom, and we'll take care of the rest. You can meet up with friends, family and other BIRD-goers from the comfort of your own home. It's safe AND fun!</p>
 </section>
 <article>
-    <a href=https://bird-rotterdam.nl/agenda/">Register hier of log in</a><br>
+    <a href="create.php">Sign up here!</a><br>
 </article>
 
 
 <section id="donate">
-    <p>To support BIRD you can donate here.</p>
+    <p>Ever since the start of the pandemic, BIRD has had to drive a rocky road. The shutdown of the venue and the restaurant meant there wasn't any income, but the monthly expenses didn't stop. BIRD luckily was able to get a loan, and they made deals with their suppliers and the government.
+    Sadly, it's not enough. Long story short: BIRD needs your help. Every donation counts. If you want to help BIRD stay in Rotterdam as a cultural hotspot, you can donate by following the link below. BIRD even offers small gestures for donating. BIRD and us thank you in advance!</p>
 </section>
 <article>
-    <a href=https://bird-rotterdam.nl/support-bird/">Check hier wat je kan doen om Bird te steunen</a><br>
+    <a href=https://bird-rotterdam.nl/support-bird/">Click here to see how you can support BIRD</a><br>
 </article>
 
 </body>
